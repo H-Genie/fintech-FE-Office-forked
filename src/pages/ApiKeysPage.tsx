62a3de3cf4a9fea810320472dd309d13a646bd64
h-genie <ses2000hj@hanmail.net>
2025-02-14 17:16:49 +0900

    feat : ApiKeysPage

--- src/pages/ApiKeysPage.tsx
+++ src/pages/ApiKeysPage.tsx
@@ -1,5 +1,10 @@
+import Issued from '@components/template/ApiKeys/Issued';
+import NotIssued from '@components/template/ApiKeys/NotIssued';
+
+const KEY_ISSUED = true;
+
 const ApiKeysPage = () => {
-  return <div>ApiKeysPage</div>;
+  return KEY_ISSUED ? <Issued /> : <NotIssued />;
 };
 
 export default ApiKeysPage;
