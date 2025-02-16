0251cd0aab08ca376aef160216a39ee6baaf0eb7
h-genie <ses2000hj@hanmail.net>
2025-02-16 22:56:21 +0900

    feat : apiClient header 설정

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -4,31 +4,59 @@ import { handleError } from './errorHandler';
 const instance = ky.create({
   prefixUrl: '/api',
   credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+    'Custom-Header': 'value',
+  },
   hooks: {
     beforeRequest: [
       (request) => {
-        request.headers.set('Content-Type', 'application/json');
+        request.headers.set('Dynamic-Header', 'dynamic-value');
       },
     ],
   },
 });
 
+// apiClient.post('/endpoint', data, {'Authorization': 'Bearer token'});
+// apiClient.get('/endpoint', {'Authorization': 'Bearer token'});
+// apiClient.put('/endpoint', data, {'Authorization': 'Bearer token'});
+// apiClient.delete('/endpoint', {'Authorization': 'Bearer token'});
 export const apiClient = {
   post: <TReq = unknown, TRes = unknown>(
     endpoint: string,
     data: TReq,
+    headers?: Record<string, string>,
   ): Promise<TRes> =>
-    instance.post(endpoint, { json: data }).json<TRes>().catch(handleError),
+    instance
+      .post(endpoint, {
+        json: data,
+        headers,
+      })
+      .json<TRes>()
+      .catch(handleError),
 
-  get: <TRes = unknown>(endpoint: string): Promise<TRes> =>
-    instance.get(endpoint).json<TRes>().catch(handleError),
+  get: <TRes = unknown>(
+    endpoint: string,
+    headers?: Record<string, string>,
+  ): Promise<TRes> =>
+    instance.get(endpoint, { headers }).json<TRes>().catch(handleError),
 
   put: <TReq = unknown, TRes = unknown>(
     endpoint: string,
     data: TReq,
+    headers?: Record<string, string>,
   ): Promise<TRes> =>
-    instance.put(endpoint, { json: data }).json<TRes>().catch(handleError),
+    instance
+      .put(endpoint, {
+        json: data,
+        headers,
+      })
+      .json<TRes>()
+      .catch(handleError),
 
-  delete: <TRes = unknown>(endpoint: string): Promise<TRes> =>
-    instance.delete(endpoint).json<TRes>().catch(handleError),
+  delete: <TRes = unknown>(
+    endpoint: string,
+    headers?: Record<string, string>,
+  ): Promise<TRes> =>
+    instance.delete(endpoint, { headers }).json<TRes>().catch(handleError),
 };
