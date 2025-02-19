272b8c371fc6daa6cb3c7b5ffab3a834a3d53d16
happyboyhj-redtable <happyboyhj@redtable.kr>
2025-02-19 13:32:13 +0900

    chore : proxy host env로 이동

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,36 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@api': path.resolve(__dirname, './src/api'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@lib': path.resolve(__dirname, './src/lib'),
-      '@mocks': path.resolve(__dirname, './src/mocks'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@type': path.resolve(__dirname, './src/types'),
-      '@schema': path.resolve(__dirname, './src/schema'),
-      '@store': path.resolve(__dirname, './src/store'),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backoffice.pay-200.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-        ws: true,
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@api': path.resolve(__dirname, './src/api'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@constants': path.resolve(__dirname, './src/constants'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+        '@lib': path.resolve(__dirname, './src/lib'),
+        '@mocks': path.resolve(__dirname, './src/mocks'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@type': path.resolve(__dirname, './src/types'),
+        '@schema': path.resolve(__dirname, './src/schema'),
+        '@store': path.resolve(__dirname, './src/store'),
       },
     },
-  },
+
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
