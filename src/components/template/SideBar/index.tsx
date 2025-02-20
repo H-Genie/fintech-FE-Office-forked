202f3f86dd2f339d6e2edefa1a9b8d16da47c4c2
h-genie <ses2000hj@hanmail.net>
2025-02-20 16:34:13 +0900

    feat : Sidebar 사용자 이름 표시

--- src/components/template/SideBar/index.tsx
+++ src/components/template/SideBar/index.tsx
@@ -1,15 +1,16 @@
 import { sideBarItems } from '@constants/sidebar.ts';
 import SideBarItem from './SideBarItem.tsx';
 import { useLocation } from 'react-router-dom';
-
+import { useAuthStore } from '@store/authStore';
 const SideBar = () => {
   const { pathname } = useLocation();
+  const { auth } = useAuthStore();
 
   return (
     <aside className='w-60 h-screen bg-[#f1f1f1] p-12'>
       <figure className='flex flex-col items-center gap-2'>
         <img src='/logo.png' className='w-8' />
-        <h4 className='font-bold text-xl truncate'>Admin</h4>
+        <h4 className='font-bold text-xl truncate'>{auth?.name}</h4>
       </figure>
 
       <nav>
