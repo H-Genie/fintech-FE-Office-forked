fbbb68ce199463240c4a32fc00448f8786f4caa2
h-genie <ses2000hj@hanmail.net>
2025-02-22 00:43:28 +0900

    fix : lint 오류 수정

--- src/mocks/handlers/key.ts
+++ src/mocks/handlers/key.ts
@@ -2,7 +2,7 @@ import { http, HttpResponse } from 'msw';
 import { API_ENDPOINTS } from '@constants/apiEndpoints';
 
 export const keyHandler = [
-  http.post(API_ENDPOINTS.KEY, () => {
+  http.post(API_ENDPOINTS.MANAGEMENT.KEYS, () => {
     return HttpResponse.json({
       ok: true,
       data: {
