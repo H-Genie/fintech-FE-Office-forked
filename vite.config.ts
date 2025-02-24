2e88e702d72286d77bcab605985c873d06fe6a3d
h-genie <ses2000hj@hanmail.net>
2025-02-25 00:48:04 +0900

    fix : production모드에서 API URL 수정(4)

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,30 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const isDevelopment = mode === 'development';
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@api': path.resolve(__dirname, './src/api'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@mocks': path.resolve(__dirname, './src/mocks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@type': path.resolve(__dirname, './src/types'),
+      '@schema': path.resolve(__dirname, './src/schema'),
+      '@store': path.resolve(__dirname, './src/store'),
+    },
+  },
 
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@api': path.resolve(__dirname, './src/api'),
-        '@components': path.resolve(__dirname, './src/components'),
-        '@constants': path.resolve(__dirname, './src/constants'),
-        '@hooks': path.resolve(__dirname, './src/hooks'),
-        '@lib': path.resolve(__dirname, './src/lib'),
-        '@mocks': path.resolve(__dirname, './src/mocks'),
-        '@pages': path.resolve(__dirname, './src/pages'),
-        '@type': path.resolve(__dirname, './src/types'),
-        '@schema': path.resolve(__dirname, './src/schema'),
-        '@store': path.resolve(__dirname, './src/store'),
+  server: {
+    proxy: {
+      '/proxy': {
+        target: 'https://backoffice.pay-200.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy/, ''),
       },
     },
-
-    server: isDevelopment
-      ? {
-          proxy: {
-            '/proxy': {
-              target: 'https://backoffice.pay-200.com',
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/proxy/, ''),
-            },
-          },
-        }
-      : undefined,
-  };
+  },
 });
