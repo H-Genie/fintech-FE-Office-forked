a6bfe718033182bd90240d5a6d78bad1900a58ef
h-genie <ses2000hj@hanmail.net>
2025-02-25 00:39:59 +0900

    fix : production모드에서 API URL 수정(2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import { API_URL } from '@constants/apiEndpoints';
+import { API_URL } from './src/constants/apiEndpoints';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
